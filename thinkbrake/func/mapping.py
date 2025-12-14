10bfe4fce0842fa13af5f9bd4b3340bf915eb681
ssangjun706 <ssangjun706@gmail.com>
2025-12-14 10:26:30 +0900

    update configs

--- thinkbrake/func/mapping.py
+++ thinkbrake/func/mapping.py
@@ -2,10 +2,10 @@ from thinkbrake.func.handler import Qwen3Handler, DeepSeekHandler, GptOssHandler
 
 MODEL_MAPPING = {
     "Qwen/Qwen3-4B-Thinking-2507": Qwen3Handler,
-    "Qwen/Qwen3-4B": Qwen3Handler,
-    "Qwen/Qwen3-14B": Qwen3Handler,
     "deepseek-ai/DeepSeek-R1-Distill-Llama-8B": DeepSeekHandler,
     "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": DeepSeekHandler,
+    "Qwen/Qwen3-4B": Qwen3Handler,
+    "Qwen/Qwen3-14B": Qwen3Handler,
     "openai/gpt-oss-20b": GptOssHandler,
     "Qwen/Qwen3-32B": Qwen3Handler,
 }
